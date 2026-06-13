c10050f242a37a6f97468f27222ed28443a5ec71
Franklin Shera <fshera96@gmail.com>
2026-06-13 17:40:20 +0300

    chore: add config constants

---
+++ src/constants/config.ts
@@ -0,0 +1,24 @@
+export const site = {
+  name: "vfshera.dev",
+  url: "https://vfshera.dev",
+  meta: {
+    title: "Franklin Shera",
+    description:
+      "A Front-End Developer committed to delivering intuitive and visually stunning websites.",
+  },
+} as const;
+
+export const socials = [
+  {
+    name: "Github",
+    link: "https://github.com/vfshera",
+  },
+  {
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/vfshera",
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/1vfshera",
+  },
+] as const;
